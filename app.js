1b5f89bf7ff9cd27690b87fdbaf04f0dfc74e119
adrewn <huuhung1999@gmail.com>
2021-08-16 13:55:31 -0400

    Implement requests, refactoring code

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const app = express();
+
+// 1) Middleware
+app.use(morgan('dev'));
+app.use(express.json());
+
+// 2) Route handlers
+
+// 3) Routes
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports=app;
